af380c033a191eaecdc3cf6e8438714275953d8e
Yakub <jorayev2494@gmail.com>
2020-03-17 17:23:34 +0200

    Modifired Auth Controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,74 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Login with get token
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function token(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $user = User::where($this->username($request), $request->get($this->username($request)))->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'The provided credentials are incorrect.'], 200);
+        }
+
+        $response = [
+            'accessToken'   => $user->createToken($request->get($this->username($request)))->plainTextToken,
+            'userData'      => $user,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username($request) => 'required|string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username(Request $request) : string
+    {
+        return $request->has('email') ? 'email' : 'login';
+    }
+
+    /**
+     * Logout with remove Tokens
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        
+        // Delete User Tokens
+        // $user->tokens()->delete();
+        $user->tokens->each->delete();
+
+        // dd($request->user());
+
+        return $this->response(response()->noContent(), 204, "logouted");
     }
 }
